3d8f25af105bd12ce5a7e9a1916df21af186251b
mischko <mhem@mailbox.org>
2024-10-26 08:47:00 +0200

    format test_utils.R

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -6,11 +6,11 @@ test_that("date_distance_fun works correctly", {
   date1 <- as.Date("2023-01-01")
   date2 <- as.Date("2023-01-02")
   date3 <- as.Date("2023-01-10")
-  
+
   # Test 1: Distance within the max_dist
   result <- date_distance_fun(date1, date2, max_dist = 2)
   expect_true(result$include) # Should be TRUE since the difference is 1 day
-  
+
   # Test 2: Distance outside the max_dist
   result <- date_distance_fun(date1, date3, max_dist = 5)
   expect_false(result$include) # Should be FALSE since the difference is 9 days
@@ -20,21 +20,21 @@ test_that("date_distance_fun works correctly", {
 # scale_this
 ################################################################################
 test_that("scale_this works correctly", {
-    # test 1: scale a vector with a mean of 0 and a standard deviation of 1
-    x <- stats::rnorm(10)
-    expect_equal(scale_this(x), as.vector(scale(x)))
-    
-    # test 2: scale a vector with a mean of 5 and a standard deviation of 2
-    x <- stats::rnorm(10, mean = 5, sd = 2)
-    expect_equal(scale_this(x), as.vector(scale(x)))
-    
-    # test 3: scale a vector with a mean of 0 and a standard deviation of 0
-    x <- rep(0, 10)
-    expect_equal(scale_this(x), as.vector(scale(x)))
-    
-    # # test 4: throw an error if the input is not numeric
-    x <- c("1")
-    expect_error(scale_this(x), "Input must be numeric")
+  # test 1: scale a vector with a mean of 0 and a standard deviation of 1
+  x <- stats::rnorm(10)
+  expect_equal(scale_this(x), as.vector(scale(x)))
+
+  # test 2: scale a vector with a mean of 5 and a standard deviation of 2
+  x <- stats::rnorm(10, mean = 5, sd = 2)
+  expect_equal(scale_this(x), as.vector(scale(x)))
+
+  # test 3: scale a vector with a mean of 0 and a standard deviation of 0
+  x <- rep(0, 10)
+  expect_equal(scale_this(x), as.vector(scale(x)))
+
+  # # test 4: throw an error if the input is not numeric
+  x <- c("1")
+  expect_error(scale_this(x), "Input must be numeric")
 })
 
 ################################################################################
@@ -47,7 +47,7 @@ test_that("lm_age generates a tidy data frame", {
     age = runif(10, 0, 100)
   )
   age_term <- lm_age(data = example_data, variable = "variable")
-  
+
   # Test 1: The output is a data frame
   expect_s3_class(age_term, "tbl_df")
   # Test 2: The output has the correct columns
@@ -78,4 +78,3 @@ test_that("my_wilcox_test works correctly", {
   expect_equal(nrow(test_result), 1)
 })
 # Create a sample dataframe
-
